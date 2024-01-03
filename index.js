8195a59bf3f63fa6ff64c344b4f92fa6e9699644
PythonXHunter <aryan.computerscience@gmail.com>
2024-01-03 11:48:42 +0530

    add env file

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const connectDB = require('./db/connect')
 const router = require('./routes/index.routes') 
 
-const port = 3000
-const mongodbUri = "mongodb://localhost/my_db"
+const port = process.env.PORT || 3000
+
 
 app.use(express.json())
 app.use('/', router);
 
 const start = async () => {
 	try {
-		await connectDB(mongodbUri);
+		await connectDB(process.env.MONGO_URI);
 		app.listen(port, () => {
 			console.log(`Server at http://localhost:${port}`)
 		})
